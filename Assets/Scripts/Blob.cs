ca52092a1cdf29f2a14610f93b8d8e181f47c7ac
George Popa <geo_007.popa@yahoo.com>
2015-11-20 18:56:09 +0000

    Food Finding
    Forgot to commit

--- Assets/Scripts/Blob.cs
+++ Assets/Scripts/Blob.cs
@@ -4,11 +4,14 @@ using Assets.Scripts;
 public class Blob : MonoBehaviour {
 
     private Rigidbody rb;
+    GameObject target;
     public float speed;
 
     float energy = 100f;
+    float range = 10f;
     float t;
     float delay = BlobManager.blobspeed;
+    int foundFood = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -17,13 +20,18 @@ public class Blob : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        rb.rotation = Quaternion.identity;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        this.GetComponentInChildren<SpriteRenderer>().color = new Color(this.energy/150, 0, 0, 1);
         if (Main.gameState == 1)
         {
             delay = BlobManager.blobspeed;
             t += Time.deltaTime * 1000;
             if (t > delay)
             {
-                Move();
+                DebugFind();
                 t = 0f;
                 energy -= 1;
             }
@@ -49,28 +57,30 @@ public class Blob : MonoBehaviour {
         }
     }
 
-    void Eat()
+    void OnCollisonEnter(Collider other)
     {
-        this.energy += 300;
+        if (other.tag == "Blob")
+        {
+            this.energy -= 10;
+        }
     }
 
-    void Move()
+    void Eat()
     {
-        int dir = Random.Range(0, 4);
-        switch (dir)
-        {
-            case 0: rb.transform.position += new Vector3(speed/10,0,0); break;
-            case 1: rb.transform.position += new Vector3(-speed/10, 0, 0); break;
-            case 2: rb.transform.position += new Vector3(0, speed/10, 0); break;
-            case 3: rb.transform.position += new Vector3(0, -speed/10, 0); break;
-        }
+        this.energy += 30;
+    }
+
+    void RandomMove()
+    {
+        float angle = Random.Range(0, 361) * Mathf.PI / 180;
+        rb.transform.position += new Vector3(speed * Mathf.Cos(angle) / 10, speed * Mathf.Sin(angle) / 10);
     }
 
     void Reproduce()
     {
-        GameObject child1 = GameObject.Instantiate(this, this.transform.position, this.transform.rotation) as GameObject;
+        GameObject child1 = GameObject.Instantiate(this, this.transform.position - new Vector3(-0.5f,-0.5f), this.transform.rotation) as GameObject;
         BlobManager.blobs.Add(child1);
-        GameObject child2 = GameObject.Instantiate(this, this.transform.position, this.transform.rotation) as GameObject;
+        GameObject child2 = GameObject.Instantiate(this, this.transform.position + new Vector3(0.5f, 0.5f), this.transform.rotation) as GameObject;
         BlobManager.blobs.Add(child2);
 
         Starve();
@@ -81,5 +91,46 @@ public class Blob : MonoBehaviour {
         BlobManager.blobs.Remove(this.gameObject);
         Destroy(this.gameObject);
         Destroy(this);
+    }
+
+    void DebugFind()
+    {
+        target = Find(float.MaxValue);
+        if (foundFood == 1)
+        {
+
+            if (target != null)
+            {
+                float angle = Mathf.Atan2((this.transform.position.y - target.transform.position.y),(this.transform.position.x - target.transform.position.x)) - Mathf.PI;
+                rb.transform.position += new Vector3(speed * Mathf.Cos(angle) / 10, speed * Mathf.Sin(angle) / 10);
+            }
+            else
+            {
+                foundFood = 0;
+            }
+        }
+        else
+        {
+            RandomMove();
+        }
+    }
+
+    GameObject Find(float min)
+    {
+        GameObject store = null;
+        foreach (GameObject food in FoodManager.foods)
+        {
+            float dist = Vector3.Distance(this.transform.position, food.transform.position);
+            if (dist < this.range)
+            {
+                if (dist < min)
+                {
+                    min = dist;
+                    foundFood = 1;
+                    store = food;
+                }
+            }
+        }
+        return store;
     }
 }
--- Assets/Scripts/FoodManager.cs
+++ Assets/Scripts/FoodManager.cs
@@ -6,6 +6,7 @@ namespace Assets.Scripts
     abstract class FoodManager
     {
         static float t = 0f;
+        static int maxFood = 50;
         public static float foodSpawnRate = 5f;
         public static float spawnspeed = Main.delay * foodSpawnRate;
         public static List<GameObject> foods = new List<GameObject>();
@@ -13,7 +14,7 @@ namespace Assets.Scripts
         public static void Spawn(GameObject food)
         {
             t += Time.deltaTime * 1000;
-            if (t > spawnspeed)
+            if (t > spawnspeed && foods.Count < maxFood)
             {
                 GameObject clone = GameObject.Instantiate(food, new Vector3(((float)Random.Range(-5000, 5000)) / 1000, ((float)Random.Range(-5000, 5000)) / 1000), new Quaternion(0, 0, 0, 0)) as GameObject;
                 foods.Add(clone);
